b7f588b90c0a600b13e5796c61b13816487cf8d7
Colby Harris <harris.colby@gmail.com>
2022-01-29 02:56:40 -0600

    added moment.js to improve task dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
+
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
@@ -13,6 +15,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -110,11 +114,23 @@ $(".list-group").on("click", "span", function(){
   $(this).replaceWith(dateInput);
 
   // automatically focus on new element
+  // dateInput.trigger("focus");
+
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    // minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  })
+
+  // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
   .val()
@@ -142,6 +158,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 $(".card .list-group").sortable({
@@ -232,21 +251,54 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+// add datepicker
+$("#modalDueDate").datepicker({
+  // minDate: 1
+});
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span")
+  .text()
+  .trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new classes if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  // this is what I did instead of the code below it
+  // else if (moment().add(3, "days").isAfter(time)) {
+  //   $(taskEl).addClass("list-group-item-warning")
+  // }
+
+  // this is their code to do the above
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning")
+  }
+};
+
 // delete task on drop
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
 
+  // not sure why event is needed to pass through here
   drop: function(event, ui) {
     ui.draggable.remove();
-    console.log("drop");
+    // console.log("drop");
   },
-  over: function(event, ui) {
-    console.log("over");
-  },
-  out: function(event, ui) {
-    console.log("out");
-  }
+  // over: function(event, ui) {
+  //   console.log("over");
+  // },
+  // out: function(event, ui) {
+  //   console.log("out");
+  // }
 })
 
 // remove all tasks
